24f404c928db5972572d059f05eb11faff82e8be
Tobias Roland <tru@charlietango.dk>
2025-08-07 15:14:40 +0200

    added rendering of images and text

--- src/blocks/SideBySide/Component.tsx
+++ src/blocks/SideBySide/Component.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import type { SideBySideBlock as SideBySideBlockProps } from '@/payload-types'
-import { cn } from '@/utilities/ui'
-
+import { Media } from '@/components/Media'
+import RichText from '@/components/RichText'
+import { richText } from 'payload/dist/fields/validations'
 export const SideBySideBlock: React.FC<SideBySideBlockProps> = ({
   heading,
   leftside,
@@ -10,35 +11,32 @@ export const SideBySideBlock: React.FC<SideBySideBlockProps> = ({
   console.log('leftside:', leftside)
   console.log('rightside:', rightside)
   return (
-    <section className={'container'}>
-      <h2>{heading}</h2>
-      <div>
+    <section className={'container '}>
+      {/*<h2>{heading}</h2>*/}
+      <div className={'grid grid-cols-2 gap-8'}>
         <div>
-          {leftside && leftside.type === 'imageside' && (
-            <div>im an left image side</div>
-            // <SideBySideImage
-            //   {...leftSide.side}
-            //   isBothSidesImage={isBothSidesImage}
-            //   otherSideEmpty={otherSideEmpty}
-            // />
+          {leftside && leftside.type === 'imageside' && leftside?.media && (
+            <Media resource={leftside.media} />
           )}
           {leftside && leftside.type === 'textside' && (
-            <div>im a left text side</div>
-            // <TextSideElement {...leftside.side} theme={theme} />
+            // <div>im a left text side</div>
+            <div>
+              <div>{leftside.title}</div>
+              {leftside.richText && (
+                <RichText className="mb-0" data={leftside.richText} enableGutter={false} />
+              )}
+            </div>
           )}
         </div>
         <div>
-          {rightside && rightside.type === 'imageside' && (
-            <div>im a right image side</div>
-            // <SideBySideImage
-            //   {...rightside.side}
-            //   isBothSidesImage={isBothSidesImage}
-            //   otherSideEmpty={otherSideEmpty}
-            // />
-          )}
+          {rightside && rightside.type === 'imageside' && <Media resource={rightside.media} />}
           {rightside && rightside.type === 'textside' && (
-            <div>im a right text side</div>
-            // <TextSideElement {...rightside.side} theme={theme} />
+            <div>
+              <div>{rightside.title}</div>
+              {rightside.richText && (
+                <RichText className="mb-0" data={rightside.richText} enableGutter={false} />
+              )}
+            </div>
           )}
         </div>
       </div>
---
+++ src/components/SideBySideImage/SideBySideImage.tsx
@@ -0,0 +1,9 @@
+import { Media } from '@/components/Media'
+import type { Props as MediaProps } from '@/components/Media/types'
+type mediaProps = {
+  media: MediaProps
+}
+
+export function SideBySideImage(media: mediaProps) {
+  return
+}
