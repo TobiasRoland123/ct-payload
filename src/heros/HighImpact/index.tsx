db18023adb7cb593b07f79a07941df899e831253
Tobias Roland <tru@charlietango.dk>
2025-10-29 09:06:38 +0100

    centered tecxt in large impact hero

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -35,13 +35,10 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, heroImages, rich
   }, [validImages.length])
 
   return (
-    <div
-      className="relative -mt-[10.4rem] flex items-center justify-center text-white bg-black"
-      data-theme="dark"
-    >
+    <div className="relative -mt-[10.4rem] flex flex-col  text-white bg-black" data-theme="dark">
       {/* Content Overlay */}
-      <div className="container mb-8 z-20 relative flex items-center justify-center">
-        <div className="max-w-[36.5rem] md:text-center">
+      <div className=" mb-8 z-20  container min-h-[80vh] flex items-center justify-center">
+        <div className=" md:text-center ">
           {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
           {Array.isArray(links) && links.length > 0 && (
             <ul className="flex md:justify-center gap-4">
@@ -58,20 +55,20 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, heroImages, rich
       </div>
 
       {/* Carousel Background */}
-      <div className="min-h-[80vh] select-none w-full z-10">
-        <div className="h-[80vh] w-full">
+      <div className="  select-none w-full z-10">
+        <div className="h-full">
           {validImages.map((media, i) => (
             <div
               key={(typeof media === 'object' && (media as any)?.id) || i}
               className={[
-                'absolute inset-0 transition-all duration-slow ease-in-out',
-                i === current ? 'opacity-100 left-0 ' : 'opacity-0 left-full',
+                'absolute inset-0 transition-all duration-slow ease-in-out object-cover w-full',
+                i === current ? 'opacity-100 left-30 ' : 'opacity-0 left-full',
               ].join(' ')}
               aria-hidden={i !== current}
             >
               <Media
                 fill
-                imgClassName="-z-10 object-cover"
+                imgClassName="-z-10 object-cover w-full"
                 priority={i === current}
                 resource={media.media}
               />
